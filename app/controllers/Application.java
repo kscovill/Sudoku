010036441e56514ba3f91e7cdc5d93ef12fa2a2e
Kyle Scovill <kscovill@carroll.edu>
2017-01-23 12:09:12 -0700

    Add Bootstrap

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("hello, world."));
+        return ok(index.render("hello, world.", play.data.Form.form(models.Task.class)));
     }
 	public static Result addTask() {
         play.data.Form<models.Task> form = play.data.Form.form(models.Task.class).bindFromRequest();
@@ -20,4 +20,5 @@ public class Application extends Controller {
         java.util.List<models.Task> tasks = new play.db.ebean.Model.Finder(String.class, models.Task.class).all();
         return ok(play.libs.Json.toJson(tasks));
     }
+	
 }
