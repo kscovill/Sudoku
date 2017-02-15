20bd27a92536bf4a260242f9452a5c61faf9b96a
Kyle Scovill <kscovill@carroll.edu>
2017-02-15 12:37:02 -0700

    working pull this one

---
+++ app/configs/DataConfig.java
@@ -0,0 +1,11 @@
+//package configs;
+//
+//import org.springframework.context.annotation.*;
+//
+//@Configuration
+//@EnableTransactionManagement
+//public class DataConfig {
+//
+//	@bean
+//	
+//}
